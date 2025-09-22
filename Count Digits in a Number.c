fead66f1a7f16377953893faf24dc4c7862d16c2
Mohammad_Sahid <shahid8975sh@gmail.com>
2025-09-22 19:53:38 +0530

    Count Digits in a Number.c

---
+++ Count Digits in a Number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    do {
+        count++;
+        num /= 10;
+    } while (num != 0);
+    printf("Digits = %d", count);
+    return 0;
+}
